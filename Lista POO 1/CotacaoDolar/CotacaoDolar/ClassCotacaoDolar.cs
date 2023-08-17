e62a0e33df17de9b9b8ddab356fb35152e9e1ac6
esmaelcoelho <69994795+esmaelcoelho@users.noreply.github.com>
2023-08-17 20:29:35 -0300

    Add files via upload

---
+++ Lista POO 1/CotacaoDolar/CotacaoDolar/ClassCotacaoDolar.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CotacaoDolar
+{
+    internal class ClassCotacaoDolar
+    {
+        internal class Multi
+        {
+            #region Atributos
+            private double b;
+            private double a;
+            private double resultado;
+            #endregion
+
+            #region MetodosInterface
+            public void setB(double p) //recebe 
+            {
+                b = p;
+            }
+            public void setA(double o) //recebe 
+            {
+                a = o;
+            }
+            public double getB()// retorna 
+            {
+                return b;
+            }
+            public double getA()
+            {
+                return a;
+            }
+            public double getresultado()//retorna resultado
+            {
+                return resultado;
+            }
+            #endregion
+            #region Método funcional
+            public void calcular()
+            {
+                resultado = a / b;
+            }
+            #endregion
+        }
+    }
+}
---
+++ Lista POO 1/CotacaoDolar/CotacaoDolar/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static CotacaoDolar.ClassCotacaoDolar;
+
+namespace CotacaoDolar
+{
+    internal class Program
+    {
+            static void Main(string[] args)
+            {
+            Multi s;
+            s = new Multi();
+            Console.WriteLine("BEM VINDO AO PROGRAMA");
+            Console.WriteLine("Entrar via teclado com o valor da cotação do dólar e uma certa quantidade de dólares.Calcular e exibir o valor correspondente em Reais (R$)");
+
+            Console.WriteLine("Digite o valor do Dolar");
+            s.setB(int.Parse(Console.ReadLine()));
+
+            Console.WriteLine("Digite o Valor para Conversão");
+            s.setA(int.Parse(Console.ReadLine()));
+
+            s.calcular();
+            Console.WriteLine("A conversao é: {2} R$", s.getB(), s.getA(), s.getresultado());
+        }
+        }
+    }
