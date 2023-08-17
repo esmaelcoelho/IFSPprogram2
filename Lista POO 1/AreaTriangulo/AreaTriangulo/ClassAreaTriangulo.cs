54763eae089cb0bc50768538941cb5ca649a89ba
esmaelcoelho <69994795+esmaelcoelho@users.noreply.github.com>
2023-08-17 20:01:59 -0300

    Add files via upload

---
+++ Lista POO 1/AreaTriangulo/AreaTriangulo/ClassAreaTriangulo.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AreaTriangulo
+{
+    internal class ClassAreaTriangulo
+    {
+        internal class Multi
+        {
+            #region Atributos
+            private int b;
+            private int a;
+            private int resultado;
+            #endregion
+
+            #region MetodosInterface
+            public void setB(int p) //recebe 
+            {
+                b = p;
+            }
+            public void setA(int o) //recebe 
+            {
+                a = o;
+            }
+            public int getB()// retorna 
+            {
+                return b;
+            }
+            public int getA()
+            {
+                return a;
+            }
+            public int getresultado()//retorna resultado
+            {
+                return resultado;
+            }
+            #endregion
+            #region Método funcional
+            public void calcular()
+            {
+                resultado = a * b /2;
+            }
+            #endregion
+        }
+        }
+    }
---
+++ Lista POO 1/AreaTriangulo/AreaTriangulo/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static AreaTriangulo.ClassAreaTriangulo;
+
+namespace AreaTriangulo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Multi s;
+            s = new Multi();
+            Console.WriteLine("BEM VINDO AO PROGRAMA");
+            Console.WriteLine("A partir dos valores da base e altura de um triângulo, calcular e exibir sua área. ");
+
+            Console.WriteLine("Digite a altura: ");
+            s.setB(int.Parse(Console.ReadLine()));
+
+            Console.WriteLine("Digite a base: ");
+            s.setA(int.Parse(Console.ReadLine()));
+
+            s.calcular();
+            Console.WriteLine("A area de um Triangulo é {2}", s.getB(), s.getA(), s.getresultado());
+        }
+    }
+}
