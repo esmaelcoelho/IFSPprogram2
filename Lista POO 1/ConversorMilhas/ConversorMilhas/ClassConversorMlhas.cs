e95bbd1f088c0d23f64723ea64db70f2b882ebab
esmaelcoelho <69994795+esmaelcoelho@users.noreply.github.com>
2023-08-17 20:14:44 -0300

    Add files via upload

---
+++ Lista POO 1/ConversorMilhas/ConversorMilhas/ClassConversorMlhas.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConversorMilhas
+{
+    internal class ClassConversorMlhas
+    {
+        internal class Multi
+        {
+            #region Atributos
+            private double a;
+            private double resultado;
+            #endregion
+
+            #region MetodosInterface
+            public void setA(int p) //recebe 
+            {
+                a = p;
+            }
+            public double getA()// retorna 
+            {
+                return a;
+            }
+            public double getresultado()//retorna resultado
+            {
+                return resultado;
+            }
+            #endregion
+            #region Método funcional
+            public void calcular()
+            {
+                resultado = 1852 * a / 1000;
+            }
+            #endregion
+        }
+    }
+}
---
+++ Lista POO 1/ConversorMilhas/ConversorMilhas/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static ConversorMilhas.ClassConversorMlhas;
+
+namespace ConversorMilhas
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Multi s;
+            s = new Multi();
+            Console.WriteLine("BEM VINDO AO PROGRAMA");
+            Console.WriteLine("Sabendo que uma milha marítima equivale a um mil, oitocentos e cinqüenta e dois metros e que um quilômetro possui mil metros, fazer um programa para converter milhas marítimas em quilômetros. ");
+
+            Console.WriteLine("Digite o valor da Milha para conversao");
+            s.setA(int.Parse(Console.ReadLine()));
+
+            s.calcular();
+            Console.WriteLine("A conversao é: {1}", s.getA(), s.getresultado());
+        }
+    }
+}
