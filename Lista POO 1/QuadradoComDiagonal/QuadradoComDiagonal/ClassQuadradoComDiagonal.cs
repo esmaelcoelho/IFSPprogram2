6a014ca73ee05ab9af557e6164d5bf581be884f1
esmaelcoelho <69994795+esmaelcoelho@users.noreply.github.com>
2023-08-17 19:51:57 -0300

    Add files via upload

---
+++ Lista POO 1/QuadradoComDiagonal/QuadradoComDiagonal/ClassQuadradoComDiagonal.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuadradoComDiagonal
+{
+    internal class ClassQuadradoComDiagonal
+    {
+        internal class classAreaRetangulo
+        {
+            internal class Multi
+            {
+                #region Atributos
+                private double diagonal;
+                private double resultado;
+                #endregion
+
+                #region MetodosInterface
+                public void setA(int p) //recebe 
+                {
+                    diagonal = p;
+                }
+                public double getA()// retorna 
+                {
+                    return diagonal;
+                }
+                public double getresultado()//retorna resultado
+                {
+                    return resultado;
+                }
+                #endregion
+                #region Método funcional
+                public void calcular()
+                {
+                    resultado = diagonal * diagonal / 2;
+                }
+                #endregion
+            }
+        }
+    }
+}
---
+++ Lista POO 1/QuadradoComDiagonal/QuadradoComDiagonal/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static QuadradoComDiagonal.ClassQuadradoComDiagonal.classAreaRetangulo;
+
+namespace QuadradoComDiagonal
+{
+            internal class Program
+        {
+            static void Main(string[] args)
+            {
+
+                Multi s;
+                s = new Multi();
+                Console.WriteLine("BEM VINDO AO PROGRAMA");
+                Console.WriteLine("Calcular e exibir a área de um quadrado a partir do valor de sua diagonal que será digitado. ");
+
+                Console.WriteLine("Digite  diagonal");
+                s.setA(int.Parse(Console.ReadLine()));
+
+                s.calcular();
+                Console.WriteLine("A área do quadrado é: {1}", s.getA(), s.getresultado());
+            }
+        }
+ }
