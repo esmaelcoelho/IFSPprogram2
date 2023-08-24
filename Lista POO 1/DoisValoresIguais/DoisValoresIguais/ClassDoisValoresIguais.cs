13548dce4eb784f9e02be54c1eda5be2750a61e5
esmaelcoelho <69994795+esmaelcoelho@users.noreply.github.com>
2023-08-24 20:50:48 -0300

    Add files via upload

---
+++ Lista POO 1/DoisValoresIguais/DoisValoresIguais/ClassDoisValoresIguais.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DoisValoresIguais
+{
+    internal class ClassDoisValoresIguais
+    {
+        internal class Multi
+        {
+            #region Atributos
+            private double b;
+            private double a;
+            private double resultado;
+            #endregion
+            #region MetodosInterface
+            public void setB(double p) //recebe 
+            {
+                this.b = p;
+            }
+            public void setA(double o) //recebe 
+            {
+                this.a = o;
+            }
+            public double getB()// retorna 
+            {
+                return this.b;
+            }
+            public double getA()
+            {
+                return this.a;
+            }
+            public double getresultado()//retorna resultado
+            {
+                return this.resultado;
+            }
+            #endregion
+            #region Método funcional
+            public void calcular()
+            {
+                if (this.b > this.a)
+                {
+                    this.resultado = this.b;
+                    Console.WriteLine("O maior número é " + this.b);
+                }
+                else if (this.a > this.b)
+                {
+                    this.resultado = this.a;
+                    Console.WriteLine("O maior número é " + this.a);
+                }
+                else if(this.b == this.a)
+                { 
+                    this.resultado = 0;
+                    Console.WriteLine("Os números são idênticos");
+                }
+            }
+            #endregion
+        }
+    }
+}
---
+++ Lista POO 1/DoisValoresIguais/DoisValoresIguais/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static DoisValoresIguais.ClassDoisValoresIguais;
+
+namespace DoisValoresIguais
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Multi s;
+            s = new Multi();
+            //s = new Multi(6,9); os Valores podem ser mencionados
+
+
+            Console.WriteLine("BEM VINDO AO PROGRAMA");
+            Console.WriteLine("Entrar com dois valores quaisquer. Exibir o maior deles, se existir, caso contrário, enviar\r\nmensagem avisando que os números são idênticos. ");
+
+            Console.WriteLine("Digite  primeiro valor: ");
+            s.setB(double.Parse(Console.ReadLine()));
+
+            Console.WriteLine("Digite segundo valor: ");
+            s.setA(double.Parse(Console.ReadLine()));
+            
+            s.calcular();
+        }
+    }
+}
