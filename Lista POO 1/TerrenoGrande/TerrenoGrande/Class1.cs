53a7c910bdfa70643abdb3d331d6c5e6da186ea5
esmaelcoelho <69994795+esmaelcoelho@users.noreply.github.com>
2023-08-24 21:02:52 -0300

    Add files via upload

---
+++ Lista POO 1/TerrenoGrande/TerrenoGrande/Class1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TerrenoGrande
+{
+    internal class Class1
+    {
+        internal class Multi
+        {
+            #region Atributos
+            private double b;
+            private double a;
+            private double resultado;
+            #endregion
+            #region MetodosInterface
+            public void setB(double p) //recebe 
+            {
+                this.b = p;
+            }
+            public void setA(double o) //recebe 
+            {
+                this.a = o;
+            }
+            public double getB()// retorna 
+            {
+                return this.b;
+            }
+            public double getA()
+            {
+                return this.a;
+            }
+            public double getresultado()//retorna resultado
+            {
+                return this.resultado;
+            }
+            #endregion
+            #region Método funcional
+            public void calcular()
+            {
+                this.resultado = a * b;
+                if (this.resultado > 100)
+                {
+                    Console.WriteLine("Terreno grande");
+                }
+                else
+                {
+                    Console.WriteLine("Terreno pequeno");
+                }
+            }
+            #endregion
+        }
+    }
+}
---
+++ Lista POO 1/TerrenoGrande/TerrenoGrande/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static TerrenoGrande.Class1;
+
+namespace TerrenoGrande
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Multi s;
+            s = new Multi();
+            //s = new Multi(6,9); os Valores podem ser mencionados
+
+
+            Console.WriteLine("BEM VINDO AO PROGRAMA");
+            Console.WriteLine("Calcular e exibir a área de um retângulo, a partir dos valores da base e altura que serão " +
+                "digitados. Se a área for maior que 100, exibir a mensagem “Terreno grande”, caso contrário, " +
+                "exibir a mensagem “Terreno pequeno");
+
+            Console.WriteLine("Digite a base ");
+            s.setB(double.Parse(Console.ReadLine()));
+
+            Console.WriteLine("Digite a altura ");
+            s.setA(double.Parse(Console.ReadLine()));
+
+            s.calcular();
+        }
+    }
+}
