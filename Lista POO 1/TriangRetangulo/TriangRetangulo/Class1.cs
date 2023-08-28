dac312b7950b1af12df22762b861997d133942f2
esmaelcoelho <69994795+esmaelcoelho@users.noreply.github.com>
2023-08-27 22:11:54 -0300

    Add files via upload
    Verificar se três valores quaisquer (A,B, C) que serão digitados formam ou não um
    triângulo retângulo.

---
+++ Lista POO 1/TriangRetangulo/TriangRetangulo/Class1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TriangRetangulo
+{
+    internal class Class1
+    {
+            internal class triangulo
+            {
+                #region Atributos
+                private double a;
+                private double b;
+                private double c;
+                #endregion
+                #region MetodosInterface
+                public void set1(double Um) //recebe 
+                {
+                    this.a = Um;
+                }
+                public void set2(double Dois) //recebe 
+                {
+                    this.b = Dois;
+                }
+                public void set3(double Tres) //recebe
+                {
+                    this.c = Tres;
+                }
+                public double get1()// retorna 
+                {
+                    return this.a;
+                }
+                public double get2()//retorna
+                {
+                    return this.b;
+                }
+                public double get3()//retorna
+                {
+                    return this.c;
+                }
+
+                #endregion
+                #region Método funcional
+                public void calcular()
+                {
+                if (this.c == this.a + this.b)
+                {
+                    Console.WriteLine("TRIANGULO RETANGULO");
+                }
+                else
+                {
+                    Console.WriteLine("Os valores não formam um TRIANGULO RETANGULO");
+                }
+            }
+        }
+        #endregion
+    }
+}
---
+++ Lista POO 1/TriangRetangulo/TriangRetangulo/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static TriangRetangulo.Class1;
+
+namespace TriangRetangulo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            triangulo s;
+            s = new triangulo();
+
+            Console.WriteLine("BEM VINDO AO PROGRAMA");
+            Console.WriteLine("Verificar se três valores quaisquer (A,B,C) que serão digitados formam ou não um" +
+            "triângulo retângulo.Lembre - se que o quadrado da hipotenusa é igual a soma dos quadrados dos" +
+            "catetos. ");
+
+            Console.WriteLine("Digite lado A cateto");
+            s.set1(double.Parse(Console.ReadLine()));
+
+            Console.WriteLine("Digite lado B cateto");
+            s.set2(double.Parse(Console.ReadLine()));
+
+            Console.WriteLine("Digite lado C hipotenusa");
+            s.set3(double.Parse(Console.ReadLine()));
+
+            s.calcular();
+
+        }
+    }
+}
