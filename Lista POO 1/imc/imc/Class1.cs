db4635703b29f6705f0ef48ffef124c805026609
esmaelcoelho <69994795+esmaelcoelho@users.noreply.github.com>
2023-08-27 21:27:38 -0300

    Add files via upload
    Entrar com o peso e a altura de uma determinada pessoa. Após a digitação, exibir se esta
    pessoa está ou não com seu peso ideal.

---
+++ Lista POO 1/imc/imc/Class1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace imc
+{
+    internal class Class1
+    {
+        internal class IMC
+        {
+            #region Atributos
+            private double peso;
+            private double altura;
+            private double imc;
+            #endregion
+            #region MetodosInterface
+            public void setPeso(double peso) //recebe 
+            {
+                this.peso = peso;
+            }
+            public void setAltura(double altura) //recebe 
+            {
+                this.altura = altura;
+            }
+            public double getPeso()// retorna 
+            {
+                return this.peso;
+            }
+            public double getAltura()
+            {
+                return this.altura;
+            }
+            public double getImc()//retorna resultado
+            {
+                return this.imc;
+            }
+            #endregion
+            #region Método funcional
+            public void calculo()
+            {
+                this.imc =  (this.peso/(this.altura * this.altura));
+
+                if (imc < 20)
+                {
+                    Console.WriteLine("Abaixo do Peso");
+                }
+                else if (imc >= 20 && imc < 25)
+                {
+                    Console.WriteLine("Peso Ideal");
+                }
+                else if (imc >= 25)
+                {
+                    Console.WriteLine("Acima do Peso");
+                }
+            }
+            #endregion
+        }
+    }
+}
---
+++ Lista POO 1/imc/imc/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static imc.Class1;
+
+namespace imc
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            IMC s;
+            s = new IMC();
+
+            Console.WriteLine("BEM VINDO AO PROGRAMA");
+            Console.WriteLine("Entrar com o peso e a altura de uma determinada pessoa. Após a digitação, exibir se esta" +
+                "pessoa está ou não com seu peso ideal. Veja tabela da relação peso/altura²");
+
+            Console.WriteLine("Digite a peso ");
+            s.setPeso(double.Parse(Console.ReadLine()));
+
+            Console.WriteLine("Digite a altura ");
+            s.setAltura(double.Parse(Console.ReadLine()));
+
+            s.calculo();
+        }
+    }
+}
