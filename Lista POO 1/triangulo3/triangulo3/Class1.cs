7a3d42a223a5d0a84ad3a6bf74ba257c4133da2d
esmaelcoelho <69994795+esmaelcoelho@users.noreply.github.com>
2023-08-27 21:53:28 -0300

    Add files via upload
    A partir de três valores que serão digitados, verificar se formam ou não um triângulo

---
+++ Lista POO 1/triangulo3/triangulo3/Class1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace triangulo3
+{
+    internal class Class1
+    {
+        internal class triangulo 
+        { 
+        #region Atributos
+        private double lado1;
+        private double lado2;
+        private double lado3;
+        #endregion
+        #region MetodosInterface
+        public void set1(double Um) //recebe 
+        {
+            this.lado1 = Um;
+        }
+        public void set2(double Dois) //recebe 
+        {
+            this.lado2 = Dois;
+        }
+        public void set3(double Tres) //recebe
+        {
+            this.lado3 = Tres;
+        }
+        public double get1()// retorna 
+        {
+            return this.lado1;
+        }
+        public double get2()//retorna
+        {
+            return this.lado2;
+        }
+        public double get3()//retorna
+        {
+            return this.lado3;
+        }
+
+        #endregion
+        #region Método funcional
+        public void calcular()
+        {
+            if ((lado1 + lado2 <= lado3) && (lado1 + lado3 <= lado2) && (lado2 + lado3 <= lado1))
+            {
+                Console.WriteLine("Não é triangulo");
+            }
+            else if ((lado1 == lado2) && (lado1 == lado3) && (lado2 == lado3))
+            {
+                Console.WriteLine("triangulo equilatero");
+            }
+            else if ((lado1 == lado2) || (lado1 == lado3) || (lado2 == lado3))
+            {
+                Console.WriteLine("isosceles");
+            }
+            else
+                Console.WriteLine("Triangulo escaleno.");
+            #endregion
+
+        }
+        }
+    }
+}
---
+++ Lista POO 1/triangulo3/triangulo3/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static triangulo3.Class1;
+
+namespace triangulo3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            triangulo s;
+            s = new triangulo();
+
+            Console.WriteLine("BEM VINDO AO PROGRAMA");
+            Console.WriteLine("A partir de três valores que serão digitados, verificar se formam ou não um triângulo");
+
+            Console.WriteLine("Digite primeiro lado ");
+            s.set1(double.Parse(Console.ReadLine()));
+
+            Console.WriteLine("Digite segundo lado ");
+            s.set2(double.Parse(Console.ReadLine()));
+
+            Console.WriteLine("Digite terceiro lado ");
+            s.set3(double.Parse(Console.ReadLine()));
+
+            s.calcular();
+
+        }
+    }
+}
