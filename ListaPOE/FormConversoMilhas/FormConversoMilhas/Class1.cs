1e7119d328818247162666aba2ec39c3ed82ba61
esmaelcoelho <69994795+esmaelcoelho@users.noreply.github.com>
2023-09-21 20:13:04 -0300

    Add files via upload

---
+++ ListaPOE/FormConversoMilhas/FormConversoMilhas/Class1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FormConversoMilhas
+{
+    internal class Class1
+    {
+        #region Atributos
+        private double a;
+        private double resultado;
+        #endregion
+
+        #region MetodosInterface
+        public void setA(int p) //recebe 
+        {
+            a = p;
+        }
+        public double getA()// retorna 
+        {
+            return a;
+        }
+        public double getresultado()//retorna resultado
+        {
+            return resultado;
+        }
+        #endregion
+        #region Método funcional
+        public void calcular()
+        {
+            resultado = 1852 * a / 1000;
+        }
+        #endregion
+    }
+}
---
+++ ListaPOE/FormConversoMilhas/FormConversoMilhas/Form1.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace FormConversoMilhas
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtNum1 = new System.Windows.Forms.TextBox();
+            this.lblResultado = new System.Windows.Forms.Label();
+            this.btmResultado = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtNum1
+            // 
+            this.txtNum1.Location = new System.Drawing.Point(132, 77);
+            this.txtNum1.Name = "txtNum1";
+            this.txtNum1.Size = new System.Drawing.Size(100, 20);
+            this.txtNum1.TabIndex = 12;
+            // 
+            // lblResultado
+            // 
+            this.lblResultado.AutoSize = true;
+            this.lblResultado.Location = new System.Drawing.Point(117, 179);
+            this.lblResultado.Name = "lblResultado";
+            this.lblResultado.Size = new System.Drawing.Size(7, 17);
+            this.lblResultado.TabIndex = 11;
+            this.lblResultado.Text = " ";
+            this.lblResultado.UseCompatibleTextRendering = true;
+            // 
+            // btmResultado
+            // 
+            this.btmResultado.Location = new System.Drawing.Point(70, 127);
+            this.btmResultado.Name = "btmResultado";
+            this.btmResultado.Size = new System.Drawing.Size(75, 23);
+            this.btmResultado.TabIndex = 10;
+            this.btmResultado.Text = "resultado";
+            this.btmResultado.UseVisualStyleBackColor = true;
+            this.btmResultado.Click += new System.EventHandler(this.btmResultado_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(51, 80);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(73, 13);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Digite a milha:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(106, 57);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(102, 13);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Conversor de milhas";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(51, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(61, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Bem Vindo!";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(292, 210);
+            this.Controls.Add(this.txtNum1);
+            this.Controls.Add(this.lblResultado);
+            this.Controls.Add(this.btmResultado);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtNum1;
+        private System.Windows.Forms.Label lblResultado;
+        private System.Windows.Forms.Button btmResultado;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
